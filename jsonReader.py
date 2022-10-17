7dd5833ff872613189db38189209c5d7e64ac202
Seungmin Jeong <seungmin1266@gmail.com>
2022-10-17 23:14:57 +0900

    Add json reading function

--- jsonReader.py
+++ jsonReader.py
@@ -1,2 +1,21 @@
 import json
 
+
+
+with open('unseJP_DB.json', 'r', encoding='utf-8') as unseDB:
+    unseData = json.load(unseDB)
+
+
+def leadingIndex(usrInput):
+    returnUnse = list()
+    returnUnse.append(unseData[usrInput - 1]['GoodOrBad'])
+    returnUnse.append(unseData[usrInput - 1]['TotalUnse'])
+    return returnUnse
+
+idxInput = int(input())
+
+leadingIndex(idxInput)
+
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from flask import Flask
+import jsonReader
 
 app = Flask(__name__)
+
+@app.route('/')
+def encodingToGB():
+    targetText = jsonReader.leadingIndex(30)
+    encoText =
+
+
+
 
 
 if __name__ == '__main__':
